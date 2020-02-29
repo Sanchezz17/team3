4c96594a1fb2e506f7ca3feccddfcd536c2056e7
etozhedankor <41688602+etozhedankor@users.noreply.github.com>
2020-02-29 16:31:10 +0500

    Implement game controllers

--- src/Controllers/GameController.cs
+++ src/Controllers/GameController.cs
@@ -1,14 +1,57 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
+using thegame.DTO;
+using thegame.Game;
+using thegame.Repositories;
 
 namespace thegame.Controllers
 {
-    [Route("api/game")]
+    [Route("api/")]
     public class GameController : Controller
     {
-        [HttpGet("score")]
-        public IActionResult Score()
+        private readonly IGameRepository gameRepository;
+        private readonly IGameFieldGenerator _gameFieldGenerator;
+
+        public GameController(IGameRepository repository, IGameFieldGenerator gameFieldGenerator)
         {
-            return Ok(50);
+            gameRepository = repository;
+            _gameFieldGenerator = gameFieldGenerator;
         }
+
+        [HttpGet("current-game")]
+        public IActionResult getCurrentGame()
+        {
+            if (Guid.TryParse(Request.Cookies["userId"], out var userId))
+            {
+                var game = gameRepository.FindByUserId(userId);
+                return Ok(game);
+            }
+
+            return NotFound();
+        }
+
+        [HttpGet("games/{gameId}")]
+        public IActionResult getGame([FromRoute] Guid gameId)
+        {
+            var game = gameRepository.FindById(gameId);
+            return Ok(game);
+        }
+
+        [HttpPost("games")]
+        [Consumes("application/json")]
+        public IActionResult CreateGame([FromBody] CreateGameParametersDTO createGameParametersDto)
+        {
+            if (!Guid.TryParse(Request.Cookies["userId"], out var userId))
+            {
+                userId = Guid.NewGuid();
+                Response.Cookies.Append("userId", userId.ToString());
+            }
+            
+            var gameField = _gameFieldGenerator.GenerateField(createGameParametersDto.Difficulty);
+            var game = new Game.Game(userId, gameField);
+            game = gameRepository.Insert(game);
+            return Ok(game);
+        }
+
     }
 }
