06edf82c2f509d2d24dc9ff73dd20a8e8a39a9bd
Евгений <gekachan2000@gmail.com>
2020-02-29 14:48:24 +0500

    Можно играть

--- src/ClientApp/components/Field/index.jsx
+++ src/ClientApp/components/Field/index.jsx
@@ -18,9 +18,37 @@ export default class Field extends React.Component {
     }
 
     makeMove = (event) => {
-        const colorName = event.target.className.split('-')[0].split('color')[1];
+        const {field} = this.state;
+        const newColor = parseInt(event.target.className.split('-')[0].split('color')[1]);
         // оптимистичный рендеринг
+        const leftCornerColor = this.state.field[0];
+        const newField = field.slice();
+        newField[0] = newColor;
+        const cellsToRepaint = [];
+        const queue = [0];
+        while (queue.length) {
+            const currentIdx = queue.shift();
+            for (const idx of this.getAdjacentCells(currentIdx)) {
+                if ((field[idx] === newColor || field[idx] === leftCornerColor) && !cellsToRepaint.includes(idx)) {
+                    cellsToRepaint.push(idx);
+                    newField[idx] = newColor;
+                    queue.push(idx);
+                }
+            }
+        }
+        this.setState({field: newField});
         // отправить запрос на бэк
+    };
+
+    getAdjacentCells = (idx) => {
+        const {width, height} = this.state;
+        return [
+            idx - 1,
+            idx + 1,
+            idx - width,
+            idx + width
+        ]
+            .filter(i => i >= 0 && i < width * height);
     };
 
     renderField = () => {
@@ -36,7 +64,8 @@ export default class Field extends React.Component {
         const {width} = this.state;
         const row = [];
         for (let i = 0; i < width; i++) {
-            row.push(<td onClick={this.makeMove} className={styles[`color${this.state.field[width * rowNumber + i]}`]}/>)
+            row.push(<td onClick={this.makeMove}
+                         className={styles[`color${this.state.field[width * rowNumber + i]}`]}/>)
         }
         return <tr>{row}</tr>;
     };
