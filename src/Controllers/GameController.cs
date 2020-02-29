1615549fe7124279320fbac7f2d418ada4a4d187
etozhedankor <41688602+etozhedankor@users.noreply.github.com>
2020-02-29 16:56:06 +0500

    Implement make turn method

--- src/Controllers/GameController.cs
+++ src/Controllers/GameController.cs
@@ -49,7 +49,13 @@ namespace thegame.Controllers
         {
             if (createGameParametersDto == null)
                 return BadRequest();
-            if (!Guid.TryParse(Request.Cookies["userId"], out var userId))
+            if (Guid.TryParse(Request.Cookies["userId"], out var userId))
+            {
+                var oldGame = gameRepository.FindByUserId(userId);
+                if (oldGame != null)
+                    gameRepository.Delete(oldGame.GameId);
+            }
+            else
             {
                 userId = Guid.NewGuid();
                 Response.Cookies.Append("userId", userId.ToString());
@@ -58,6 +64,27 @@ namespace thegame.Controllers
             var gameField = _gameFieldGenerator.GenerateField(createGameParametersDto.Difficulty);
             var game = new Game.Game(userId, gameField);
             game = gameRepository.Insert(game);
+            return Ok(game);
+        }
+
+        [HttpPost("games/{gameId}")]
+        [Consumes("application/json")]
+        public IActionResult MakeMove([FromRoute] Guid gameId, [FromBody] GameMoveDto gameMoveDto)
+        {
+            if (gameMoveDto == null || gameId == Guid.Empty)
+                return BadRequest();
+            var game = gameRepository.FindById(gameId);
+            if (game == null)
+                return NotFound();
+            if (Guid.TryParse(Request.Cookies["userId"], out var userId))
+            {
+                if (userId == game.UserId)
+                {
+                    game.MakeTurn(gameMoveDto.Color);
+                    gameRepository.Update(game);
+                }
+            }
+
             return Ok(game);
         }
     }
